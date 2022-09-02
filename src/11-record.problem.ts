d58f27cea5575ab542a082b1d0ecb96f72559e84
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-09-02 15:55:02 -0400

    typeof narrowing & typeguard

--- src/11-record.problem.ts
+++ src/11-record.problem.ts
@@ -1,7 +1,16 @@
 import { expect, it } from "vitest";
 
+interface CacheInterface {
+  [id: string]: string;
+}
+
 const createCache = () => {
-  const cache = {};
+  // solution
+  // const cache: Record<string, string> = {};
+  // solution 2
+  // const cache: { [key: string]: string } = {};
+  //solution 3
+  const cache: CacheInterface = {};
 
   const add = (id: string, value: string) => {
     cache[id] = value;
@@ -24,6 +33,8 @@ it("Should add values to the cache", () => {
   cache.add("123", "Matt");
 
   expect(cache.cache["123"]).toEqual("Matt");
+  cache.remove("123");
+  expect(cache.cache["123"]).toEqual(undefined);
 });
 
 it("Should remove values to the cache", () => {
@@ -34,3 +45,43 @@ it("Should remove values to the cache", () => {
 
   expect(cache.cache["123"]).toEqual(undefined);
 });
+
+const createCache2 = () => {
+  // an object that can have keys and values both as strings
+  const cache: Record<number, string> = {};
+
+  // Method to add key
+  const add = (id: number, value: string) => {
+    cache[id] = value;
+  };
+
+  // Method to remove a key,value pair
+  const remove = (id: number) => {
+    delete cache[id];
+  };
+
+  return {
+    cache,
+    add,
+    remove,
+  };
+};
+
+// Using partial
+interface Todo {
+  id: number;
+  task: string;
+}
+
+function addTodo(todo: Todo): Todo {
+  return todo;
+}
+function updateTodo(todo: Todo, parts: Partial<Todo>): Todo {
+  return { ...todo, ...parts };
+}
+
+const task1: Todo = { id: 1, task: "take dog for a walk" };
+
+const updated = updateTodo(task1, { task: "take sunny to walk" });
+console.log("task1 = ", task1);
+console.log("updated = ", updated);
--- src/12-typeof-narrowing.problem.ts
+++ src/12-typeof-narrowing.problem.ts
@@ -1,11 +1,26 @@
 import { expect, it } from "vitest";
 
-const coerceAmount = (amount: number | { amount: number }) => {};
+const coerceAmount = (
+  amount: number | { amount: number; something: number }
+) => {
+  if (typeof amount === "number") {
+    return amount;
+  }
+  return [amount.amount, amount.something];
+};
 
 it("Should return the amount when passed an object", () => {
-  expect(coerceAmount({ amount: 20 })).toEqual(20);
+  expect(coerceAmount({ amount: 20, something: 21 })).toEqual([20, 21]);
 });
 
 it("Should return the amount when passed a number", () => {
   expect(coerceAmount(20)).toEqual(20);
 });
+
+const addPadding = (strs: string | string[]) => {
+  if (typeof strs === "object") {
+    for (const s of strs) {
+      console.log(s);
+    }
+  }
+};
